a76c28be5785cf1a95ae0a8f9c9abcfae242d517
Ben Le <lebenjamin77@yahoo.com>
2024-02-19 23:06:22 -0700

    Delete src/env.tsx

--- src/env.tsx
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  CTFL_SPACE_ID: "4163m42u0lcs",
-  CTFL_ENV_ID: "master",
-  //CTFL_API_HOST:
-  CTFL_TOKEN: "-u47xvK3VB7gB4FfZQzcaq-Sccs3LWpzpkbBHArpMak",
-  REACT_APP_CTFL_EXPERIENCE_TYPE_ID: "benEb",
-};
